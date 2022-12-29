33c61ff96bbd2cb911d3b78e580627827ea19c01
Rahul_Nandy <98167232+20Rahu01@users.noreply.github.com>
2022-12-29 19:58:19 +0530

    Create Pattern1.java

---
+++ 29th_12_2022/Pattern1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String []args){
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+	    n =sc.nextInt();
+	    
+//	    * 
+//	    * * 
+//	    * * * 
+//	    * * * * 
+//	    * * * * * 
+	    for(int i=1;i<=n;i++) { //for row
+	    	
+	    	for(int j=1;j<=i;j++) //for column
+	    		System.out.print("* ");
+	    	
+	    	System.out.println(""); // printing new line
+	    }
+	    
+//	    * * * * 
+//	    * * * 
+//	    * * 
+//	    * 
+		for(int i=1;i<=n;i++) { //for row
+			    	
+	    	for(int j=n;j>i;j--) //for column
+	    		System.out.print("* ");
+	    	
+	    	System.out.println(""); // printing new line
+	    }
+	    
+	}
+}
